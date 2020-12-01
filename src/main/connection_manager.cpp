e7e60183a2c58e5bf2f6092e0605b9c3aa408669
eric <eric.j.isaac@gmail.com>
2020-11-30 23:53:17 -0800

    increase buffer size, correct EOF

--- src/main/connection_manager.cpp
+++ src/main/connection_manager.cpp
@@ -499,6 +499,8 @@ namespace simularium {
             );
       } catch (...) {
           this->LogClientEvent(connectionUID, "Failed to send websocket message to client");
+          LOG_F(ERROR, "Websocket send failed with exception, removing offending connection...");
+          this->RemoveConnection(connectionUID);
       }
     }
 
