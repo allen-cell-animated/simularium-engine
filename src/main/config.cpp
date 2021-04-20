469845c3be1cc4229637d03ed9e4386545e28447
eric <eric.j.isaac@gmail.com>
2021-04-19 19:46:55 -0700

    correct following / on cache folder

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -3,7 +3,7 @@
 namespace aics {
 namespace simularium {
 namespace config {
-  std::string GetCacheFolder() { return "/tmp/aics/simularium"; }
+  std::string GetCacheFolder() { return "/tmp/aics/simularium/"; }
   std::string GetS3Location() { return "trajectory/"; }
   std::string GetS3CacheLocation() { return  "cache/trajectory/" + GetEnvironment() + "/"; }
   std::string GetEnvironment() { return std::getenv("APP_ENVIRONMENT"); }
