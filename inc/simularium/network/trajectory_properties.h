b6800a528f40fc5ded457e95633fe7f121add611
eric <eric.j.isaac@gmail.com>
2021-05-05 18:01:57 -0700

    format

--- inc/simularium/network/trajectory_properties.h
+++ inc/simularium/network/trajectory_properties.h
@@ -1,35 +1,36 @@
 #ifndef AICS_TRAJECTORY_PROPERTIES_H
 #define AICS_TRAJECTORY_PROPERTIES_H
 
-#include <string>
 #include <array>
+#include <string>
 #include <unordered_map>
 
 namespace aics {
 namespace simularium {
 
-  struct CameraPosition {
-    CameraPosition() {
-      position = { 0, 0, 120 };
-      lookAt = { 0, 0, 0 };
-      upVector = { 0, 1, 0 };
-      fov = 50;
-    }
-    
-    std::array<float, 3> position;
-    std::array<float, 3> lookAt;
-    std::array<float, 3> upVector;
-    float fov;
-  };
-  
-struct TrajectoryFileProperties{
-    std::string fileName = "";
-    std::size_t numberOfFrames = 0;
-    double timeStepSize = 100;
-    float spatialUnitFactorMeters = 1e-9;
-    std::unordered_map<std::size_t, std::string> typeMapping;
-    float boxX, boxY, boxZ;
-    CameraPosition cameraDefault;
+    struct CameraPosition {
+        CameraPosition()
+        {
+            position = { 0, 0, 120 };
+            lookAt = { 0, 0, 0 };
+            upVector = { 0, 1, 0 };
+            fov = 50;
+        }
+
+        std::array<float, 3> position;
+        std::array<float, 3> lookAt;
+        std::array<float, 3> upVector;
+        float fov;
+    };
+
+    struct TrajectoryFileProperties {
+        std::string fileName = "";
+        std::size_t numberOfFrames = 0;
+        double timeStepSize = 100;
+        float spatialUnitFactorMeters = 1e-9;
+        std::unordered_map<std::size_t, std::string> typeMapping;
+        float boxX, boxY, boxZ;
+        CameraPosition cameraDefault;
 
         std::string Str()
         {
